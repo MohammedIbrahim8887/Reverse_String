0725e4c957bcea8d7cb0aca9f001a11fbc98d98c
MohammedIbrahim8887 <95257247+MohammedIbrahim8887@users.noreply.github.com>
2022-09-25 20:21:23 +0300

    Reverse a word that is 5 words or above. works both for sentence and single words.

---
+++ src/reverse_word.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class reverse_word {
+    public static void main(String[] args) {
+        take_word();
+    }
+    public static void take_word() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter word");
+        String word = in.nextLine();
+        String[] word1 = word.split(" ");
+        for(int i=0;i<word.length();i++){
+            if(word1[i].length()>=5){
+                char[] words = word1[i].toCharArray();
+                char letters;
+                int j = word1[i].length();
+                while (j > 0) {
+                    letters = words[j - 1];
+                    System.out.print(letters);
+                    j--;
+                }
+                System.out.print(" ");
+            }
+            else
+                System.out.print(word1[i] + " ");
+        }
+    }
+}
